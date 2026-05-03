a4e25e1c41cf5211cc00c4fac3aef897b4673e88
KGFCH2 <babinbid05@gmail.com>
2026-05-03 19:46:19 +0530

    Finalized the application entry point with updated theme and context providers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
+import { ThemeProvider } from './context/ThemeContext.jsx'
+import { EmergencyProvider } from './context/EmergencyContext.jsx'
 import './index.css'
 
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || '240882103415-s5f6hkpd7ss5pj55at4vodsdaomup7v1.apps.googleusercontent.com';
@@ -11,10 +13,14 @@ const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || '240882103415-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      <BrowserRouter>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
+      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <ThemeProvider>
+          <AuthProvider>
+            <EmergencyProvider>
+              <App />
+            </EmergencyProvider>
+          </AuthProvider>
+        </ThemeProvider>
       </BrowserRouter>
     </GoogleOAuthProvider>
   </React.StrictMode>
